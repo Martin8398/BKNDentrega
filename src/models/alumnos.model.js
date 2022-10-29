08a0490a4fd94ab578849be2f8795c17d79396b1
TinchoLarrosa <martinnicolas.larrosa@gmail.com>
2022-10-29 03:02:48 -0300

    fixes

--- src/models/alumnos.model.js
+++ src/models/alumnos.model.js
@@ -1,13 +1,13 @@
 const { Schema, model } = require("mongoose");
 
 const AlumnoSchema = new Schema({
-    name: { type: String, required: [true, "No puede no tener un nombre"] },
-    lastName: { type: String, required },
-    age: { type: Number, required },
+    name: { type: String, required: [true, "Por favor coloque un nombre"] },
+    lastName: { type: String, required : [true, "Por favor coloque un apellido"] },
+    age: { type: Number, required : [true, "Por favor coloque una edad"]},
     id: { type: Number, require: true, unique: true },
     specialization: { type: String, enum: ["Cirujia", "Pediatria", "Quinesiologia", "Cardiologia","Proctologia"], require: true }
-});
+})
 
 const AlumnoModel = model("Alumnos", AlumnoSchema);
 
-module.exports = UsuarioModel;
+module.exports = AlumnoModel;
